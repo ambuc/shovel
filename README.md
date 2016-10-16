984c58c0bf9a25be658d6073e581043f1804a369
James Adam Buckland <james.adam.buckland@gmail.com>
2016-10-15 23:20:43 -0500

    first commit

--- README.md
+++ README.md
@@ -1,2 +1,58 @@
 # shovel
 Use this to dig yourself out of the proverbial hole.
+
+## Student loan payment scheduler
+
+```javascript
+$ python shovel.py 
++-----------------+---------+---------+---------+--------+---------+
+|                 |  Loan01 |  Loan02 |  Loan03 | Loan04 |   Total |
++-----------------+---------+---------+---------+--------+---------+
+|                 |         |         |         |        |         |
+|  2017 Principal | 1000.00 |  500.00 |  100.00 | 100.00 | 1700.00 |
+|        Interest |  +50.00 |  +75.00 |  +40.00 |  +5.00 |  170.00 |
+| Monthly Payment |  -24.00 |  -36.00 |  -11.00 |  -2.00 |   73.00 |
+|  Annual Payment | -294.00 | -441.00 | -140.00 | -29.00 |  904.00 |
+|                 |         |         |         |        |         |
+|  2018 Principal |  756.00 |  134.00 |    0.00 |  76.00 |  966.00 |
+|        Interest |  +37.80 |  +20.10 |   +0.00 |  +3.80 |   61.70 |
+| Monthly Payment |  -51.00 |  -12.00 |   -0.00 |  -5.00 |   68.00 |
+|  Annual Payment | -612.00 | -154.00 |   -0.00 | -61.00 |  827.00 |
+|                 |         |         |         |        |         |
+|  2019 Principal |  181.80 |    0.00 |    0.00 |  18.80 |  200.60 |
+|        Interest |   +9.09 |   +0.00 |   +0.00 |  +0.94 |   10.03 |
+| Monthly Payment |  -15.00 |   -0.00 |   -0.00 |  -1.00 |   16.00 |
+|  Annual Payment | -190.00 |   -0.00 |   -0.00 | -19.00 |  209.00 |
++-----------------+---------+---------+---------+--------+---------+
+```
+
+The `shovel.py` script reads a coniguration file, `config.yaml`.
+
+```yaml
+period: Yearly          #recalculation frequency. "Yearly" or "Monthly".
+
+# If your period is monthly, 
+#  - your starting payment should be in terms of how much per month you can afford, and
+#  - your growth           should be in terms of how much salary growth you expect each month.
+
+startingPayment: 1000   #how much to begin paying down initially per period
+growth: 0               #by what percent to increase the payment each period.
+rounding: True          #whether or not to round loan payments to the nearest dollar
+
+startingYear: 2017      #the year repayment started. 
+startingMonth: 1        #the month repayment started.
+
+loans: 
+  - name: Loan01        #use unique identifiers please :) 
+    prin: 1000          #principal amount of loan
+    rate: 5.00          #rate on loan
+  - name: Loan02
+    prin: 500
+    rate: 15.00
+  - name: Loan03
+    prin: 100
+    rate: 40.00
+  - name: Loan04
+    prin: 100
+    rate: 5.00
+```
---
+++ shovel.py
@@ -0,0 +1,132 @@
+import yaml
+from prettytable import PrettyTable
+import prettytable
+import calendar
+
+class bcolors:
+    HEADER = '\033[95m'; OKBLUE = '\033[94m'; OKGREEN = '\033[92m';   WARNING = '\033[93m'
+    FAIL   = '\033[91m';   ENDC = '\033[0m';     BOLD = '\033[1m';  UNDERLINE = '\033[4m'
+
+def getInterest(prin, rate):
+    if period == "Yearly":
+        return prin*(1+rate)-prin
+    else: #Monthly
+        return prin*(1+(rate/12))-prin
+
+def calcWeights(loans):
+    interests, weights = [], []
+    [ interests.append( getInterest(l['prin'], l['rate']) ) for l in loans ]
+    total = sum(interests)
+    [ weights.append( i / total ) for i in interests ]
+    return weights
+
+def calcPayments(loans, payment):
+    payments = []
+    weights = calcWeights(loans)
+    for i in range(len(loans)):
+	#we take the minimum of our recommended weighting and the potential accumulation of the next year
+        #this lets us surge a bit on the last year to tackle any remaining interest
+        p = min( loans[i]['prin'] + getInterest( loans[i]['prin'], 
+                                                 loans[i]['rate'] ), 
+                  weights[i]*payment)
+	if rounding:
+            p = int(p)
+	payments.append(p)
+    return payments
+
+def increment(loans, payment):
+    payments = calcPayments(loans, payment)
+    for i in range(len(loans)):
+        loan = loans[i]
+        loan['prin'] = loan['prin']+getInterest(loan['prin'], loan['rate']) - payments[i]
+        if loan['prin'] < 10:
+            loan['prin'] = 0
+    return loans, payments
+
+def pNeg(n):
+    if n == 0: return format(n, ".2f")
+    else: return bcolors.FAIL + format(n, ".2f") + bcolors.ENDC
+
+def pPos(n):
+    if n == 0: return format(n, ".2f")
+    else: return bcolors.OKGREEN + format(n, ".2f") + bcolors.ENDC
+
+def pWarn(n): return bcolors.WARNING+n+bcolors.ENDC
+def pOK(n): return bcolors.OKBLUE+n+bcolors.ENDC
+def pHeader(n): return bcolors.HEADER+n+bcolors.ENDC
+
+def newLine(n):
+    return [" " for i in range(n)]
+
+def calcLeft(loans):
+    return sum([l['prin'] for l in loans])
+
+def schedule(loans):
+    payment, month = startingPayment, startingMonth
+    year = 0
+
+    t = PrettyTable([""]+[pHeader(l['name']) for l in loans]+[pHeader("Total")], align="r")
+
+    #alignments
+    for l in loans:
+        t.align[pHeader(l['name'])] = "r"
+    t.align[""] = "r"
+    t.align[pHeader("Total")] = "r"
+
+    while calcLeft(loans) > 10:
+        t.add_row(newLine(2+len(loans)))
+
+        if period == "Monthly":
+            yearTitle = "{0} {1}".format(calendar.month_abbr[month], year+startingYear)
+            periodPaymentTitle = pHeader("Monthly")+" Payment"
+        else:
+            yearTitle = "{0}".format(year+startingYear)
+            periodPaymentTitle = pHeader("Annual")+" Payment"
+
+        t.add_row(["{0} Principal".format(pWarn(yearTitle))]
+                + [pPos(l['prin']) for l in loans]
+                + [format(sum([l['prin'] for l in loans]),".2f")])
+
+        t.add_row(["Interest"]
+                + ["+{0}".format(format(getInterest(l['prin'], l['rate']),".2f")) for l in loans]
+                + [format(sum([getInterest(l['prin'], l['rate']) for l in loans]),".2f")])
+
+        loans, recentPayments  = increment(loans, payment)
+
+        if period == "Yearly":
+            t.add_row(["Monthly Payment"]
+                    + ["-{0}".format(format(r/12,".2f")) for r in recentPayments]
+                    + [format(sum([r/12 for r in recentPayments]),".2f")])
+
+        t.add_row([periodPaymentTitle]
+                + ["-{0}".format(pNeg(r)) for r in recentPayments]
+                + [pHeader(format(sum([r for r in recentPayments]),".2f"))])
+
+        payment = payment * (1 + growth)
+
+        if period == "Monthly":
+            if month == 12:
+                month = 1; year = year + 1
+            else:
+                month = month + 1
+        else:
+            year = year + 1
+
+    print t
+
+y = yaml.load(open('config.yaml'))
+
+global rounding, period, growth, startingYear, startingMonth, startingPayment
+rounding =  y['rounding']
+period = y['period'] or "Yearly"
+growth = float(y['growth'])/100 or 0
+startingYear = y['startingYear']
+startingMonth = y['startingMonth']
+startingPayment = float(y['startingPayment']) or 5000
+
+loans = y['loans']
+
+for loan in loans:
+    loan['rate'] /= 100 #percent to decimal
+
+schedule(loans)
